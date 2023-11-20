59970019ec0ded631470aa478d659767365c353c
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-11-20 19:09:06 -0300

    Added base properties to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        offwhite: '#CBD2D0',
+        black: '#080708',
+        white: '#FFFDFD',
+        primary: '#F0E100',
+        secondary: '#3772FF',
+      },
+    },
+    fontSize: {
+      base: ['1rem', '1rem'],
     },
   },
   plugins: [],
