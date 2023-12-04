ad8123e22e3e2830e4f8c8d102105669b2a748e8
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-04 08:45:41 -0300

    Added migrate file

---
+++ src/lib/db/migrate.ts
@@ -0,0 +1,22 @@
+import { PlanetScaleDatabase } from 'drizzle-orm/planetscale-serverless';
+import { MySql2Database } from 'drizzle-orm/mysql2';
+import { migrate as psMigrate } from 'drizzle-orm/planetscale-serverless/migrator';
+import { migrate as mysqlMigrate } from 'drizzle-orm/mysql2/migrator';
+import { db } from './index';
+import { ENVIRONMENT } from '../../../config';
+
+const connectionConfig = {
+  migrationsFolder: './migrations',
+};
+
+if (ENVIRONMENT === 'production') {
+  await psMigrate(
+    db as PlanetScaleDatabase<Record<string, never>>,
+    connectionConfig
+  );
+} else {
+  await mysqlMigrate(
+    db as MySql2Database<Record<string, never>>,
+    connectionConfig
+  );
+}
