fd42c85f425ba4b43a9867c28dbf5b9a2a83c172
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-04 08:43:44 -0300

    Added config file

--- config.ts
+++ config.ts
@@ -1,6 +1,38 @@
-export const {
+export const { NODE_ENV } = process.env;
+
+export const ENVIRONMENT = NODE_ENV || 'development';
+
+export const getEnvVariable = (key: string) => {
+  let prefix;
+
+  switch (ENVIRONMENT) {
+    case 'development':
+      prefix = 'DB';
+      break;
+    case 'test':
+      prefix = 'TESTING_DB';
+      break;
+    case 'production':
+      prefix = 'STAGING_DB';
+      break;
+    default:
+      prefix = 'DB';
+  }
+
+  const prefixedKey = `${prefix}_${key}`.toUpperCase();
+  return process.env[prefixedKey];
+};
+
+const DATABASE_HOST = getEnvVariable('HOST');
+const DATABASE_NAME = getEnvVariable('NAME');
+const DATABASE_USERNAME = getEnvVariable('USERNAME');
+const DATABASE_PASSWORD = getEnvVariable('PASSWORD');
+const DATABASE_PORT = Number(getEnvVariable('PORT'));
+
+export {
   DATABASE_HOST,
   DATABASE_NAME,
   DATABASE_USERNAME,
   DATABASE_PASSWORD,
-} = process.env;
+  DATABASE_PORT,
+};
