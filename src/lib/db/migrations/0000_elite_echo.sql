47583a6ceff570a338af526c5f34506df481c24b
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-10 16:11:50 -0300

    New migrations

--- src/lib/db/migrations/0000_elite_echo.sql
+++ src/lib/db/migrations/0000_elite_echo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `snk_ecommerce_shoeImages` (
-	`id` bigint unsigned AUTO_INCREMENT NOT NULL,
+	`id` serial AUTO_INCREMENT NOT NULL,
 	`shoe_id` int,
 	`image_url` varchar(255) NOT NULL,
 	`created_at` timestamp DEFAULT (now()),
@@ -8,7 +8,7 @@ CREATE TABLE `snk_ecommerce_shoeImages` (
 );
 --> statement-breakpoint
 CREATE TABLE `snk_ecommerce_shoeStock` (
-	`id` int AUTO_INCREMENT NOT NULL,
+	`id` serial AUTO_INCREMENT NOT NULL,
 	`shoe_id` int,
 	`size` varchar(10) NOT NULL,
 	`stock` int NOT NULL DEFAULT 0,
@@ -18,7 +18,7 @@ CREATE TABLE `snk_ecommerce_shoeStock` (
 );
 --> statement-breakpoint
 CREATE TABLE `snk_ecommerce_shoes` (
-	`id` int AUTO_INCREMENT NOT NULL,
+	`id` serial AUTO_INCREMENT NOT NULL,
 	`name` varchar(255) NOT NULL,
 	`description` text,
 	`price` decimal(10,2),
@@ -26,6 +26,3 @@ CREATE TABLE `snk_ecommerce_shoes` (
 	`updated_at` timestamp DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
 	CONSTRAINT `snk_ecommerce_shoes_id` PRIMARY KEY(`id`)
 );
---> statement-breakpoint
-ALTER TABLE `snk_ecommerce_shoeImages` ADD CONSTRAINT `snk_ecommerce_shoeImages_shoe_id_snk_ecommerce_shoes_id_fk` FOREIGN KEY (`shoe_id`) REFERENCES `snk_ecommerce_shoes`(`id`) ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE `snk_ecommerce_shoeStock` ADD CONSTRAINT `snk_ecommerce_shoeStock_shoe_id_snk_ecommerce_shoes_id_fk` FOREIGN KEY (`shoe_id`) REFERENCES `snk_ecommerce_shoes`(`id`) ON DELETE no action ON UPDATE no action;
