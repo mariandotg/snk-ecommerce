c94825722ec61291e014deca6eb81251fbfbe736
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-26 14:08:57 -0300

    Added base zod validation on userSchema

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Input from './Input';
-import { UseFormRegisterReturn } from 'react-hook-form';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 import { LoginFormValues } from '@/models/LoginFormValues';
 
 interface Props {
   registerFn: UseFormRegisterReturn<keyof LoginFormValues>;
   value: keyof LoginFormValues;
+  error: FieldError | undefined;
 }
 
-const Field = ({ registerFn, value }: Props) => {
+const Field = ({ registerFn, value, error }: Props) => {
   return (
     <label className='flex flex-col gap-y-2'>
       {value.toLocaleUpperCase()}
       <Input registerFn={registerFn} />
+      {error && (
+        <p className='text-xs italic text-red-500 mt-2'>{error?.message}</p>
+      )}
     </label>
   );
 };
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,15 +3,40 @@ import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { LoginFormValues } from '@/models/LoginFormValues';
 import Field from './Field';
+import { z } from 'Zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const userSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+  .required();
 
 const LoginForm = () => {
-  const { register, handleSubmit } = useForm<LoginFormValues>();
-  const onSubmit: SubmitHandler<LoginFormValues> = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(userSchema),
+  });
+  const onSubmit: SubmitHandler<LoginFormValues> = (data) => {
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4'>
-      <Field value='email' registerFn={register('email')} />
-      <Field value='password' registerFn={register('password')} />
+      <Field
+        value='email'
+        registerFn={register('email')}
+        error={errors.email}
+      />
+      <Field
+        value='password'
+        registerFn={register('password')}
+        error={errors.password}
+      />
       <input
         type='submit'
         className='bg-primary text-black w-fit px-3 py-1 rounded-sm'
