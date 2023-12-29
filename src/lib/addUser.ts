0af86df75371d0281b8843cc65c6f3892349bbaa
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-28 22:26:01 -0300

    Added encryption to addUser action

--- src/lib/addUser.ts
+++ src/lib/addUser.ts
@@ -2,7 +2,18 @@
 import { RegisterFormValues } from '@/models/RegisterFormValues';
 import { db } from './db';
 import { users } from './db/schema';
+import bcrypt from 'bcrypt';
 
 export default async function addUser(values: RegisterFormValues) {
-  await db.insert(users).values(values);
+  const { password, email } = values;
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  await db.insert(users).values({
+    email,
+    password: hashedPassword,
+  });
 }
