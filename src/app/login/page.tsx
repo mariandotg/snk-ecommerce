df2b33152ac95568cd5fe3549656f9585e3009fe
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-28 22:27:34 -0300

    Added LoginPage

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/LoginForm';
+import React from 'react';
+
+interface Props {}
+
+export default function LoginPage({}: Props) {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
