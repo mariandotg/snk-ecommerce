2c238b7848f57237ba95fbf05c67891bc931ef32
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-12-30 01:47:07 -0300

    Added password on users table migration

---
+++ src/lib/db/migrations/0004_uneven_post.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `snk_ecommerce_users` ADD `password` varchar(255) NOT NULL;
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -44,6 +44,7 @@ export const users = mysqlTable('users', {
   id: varchar('id', { length: 255 }).notNull().primaryKey(),
   name: varchar('name', { length: 255 }),
   email: varchar('email', { length: 255 }).notNull(),
+  password: varchar('password', { length: 255 }).notNull(),
   emailVerified: timestamp('emailVerified', {
     mode: 'date',
     fsp: 3,
